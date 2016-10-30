794b24bccf031398571c0a9a6ae9266c3eb1b162
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2016-10-30 20:42:55 +0800

    Clean PokemonItem['height_feet'] and ['height_inches']

--- pokedex_scraper/pipelines.py
+++ pokedex_scraper/pipelines.py
@@ -9,6 +9,7 @@ import re
 class PokemonPipeline(object):
     def process_item(self, item, spider):
         item['eng_name'] = re.sub(r'.* ', '', item['eng_name'])
-        
+        item['height_feet'] = int(item['height_feet'])
+        item['height_inches'] = int(item['height_inches'].replace('"', ''))
 
         return item
